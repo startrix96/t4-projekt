9ba9a7d235d5e2f2614cfd21576ab23936a0c9c1
Benjamin Porsgaard <benjamin_porsgaard@hotmail.com>
2022-05-10 13:27:48 +0200

    fjernet forside link i header

--- src/components/SiteNavbar.vue
+++ src/components/SiteNavbar.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="topnav">
         <a class="active" href="#home">Forside</a>
-       
-            <nav>
-        <router-link to="/">Forside</router-link> |
-        <router-link to="../views/ShopView">Shop</router-link>
-        <router-link to="../views/ContactView">contact</router-link>
-        <router-link to="../views/LoginView">login</router-link>
-        <router-link to="../views/CartView">cart</router-link>
-        
-        
-    </nav>
-    <router-view />
+
+        <nav>
+            <router-link to="../views/ShopView">Shop</router-link>
+            <router-link to="../views/ContactView">contact</router-link>
+            <router-link to="../views/LoginView">login</router-link>
+            <router-link to="../views/CartView">cart</router-link>
+        </nav>
+        <router-view />
 
         <input type="text" placeholder="Search.." />
     </div>
