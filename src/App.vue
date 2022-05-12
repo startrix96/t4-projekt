b73e80b72e0379a8024c6dbe2aa7ed0e789d8449
Laurits92 <34508820+Laurits92@users.noreply.github.com>
2022-05-12 14:00:10 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
-
-
     <SiteNavbar />
     <SiteHeader />
     <SiteFooter />
-    <div class="productgrid">
-        <div class="product" v-for="product in products" :key="product.id">
-            <p class="price">{{ product.attributes.price }} kr</p>
-            <h3 class="title">{{ product.attributes.title }}</h3>
-        </div>
-    </div>
 </template>
 
 <script>
@@ -24,43 +16,9 @@ export default {
         SiteHeader,
         SiteFooter,
     },
-    data() {
-        return {
-            products: "",
-        };
-    },
-
-    mounted() {
-        fetch("https://backend-mark.herokuapp.com/api/products/")
-            .then(response => response.json())
-            .then(data => (this.products = data.data));
-    },
 };
 </script>
 
 <style lang="scss">
 @import "./assets/scss/app.scss";
-
-.productgrid {
-    display: flex;
-    flex-wrap: wrap;
-}
-
-.product {
-    height: 200px;
-    background-color: lightgray;
-    margin: 40px 40px;
-    max-width: 100%;
-    width: 28%;
-    h3 {
-        font-size: 20px;
-        text-align: center;
-    }
-}
-.price {
-    text-align: center;
-    color: #ed5c00;
-    font-weight: 600;
-    font-size: 18px;
-}
 </style>
