433358e5ac1dc61bd2a516ab7f143a19c7ae260b
Laurits92 <34508820+Laurits92@users.noreply.github.com>
2022-05-12 08:15:35 +0200

    footer

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="SiteFooter"> 
+        
+        <div class="footer">
+        <!-- <a class= "footer__logo" href="#">
+            <img src="/images/logo.svg" />
+        </a> -->
+        <div class="logo-placeholder">
+<h1>logo</h1>
+        </div>
+        <div class="footer__department">
+            <h2>Afdelinger</h2>
+            <ul class="department__list">
+            <li><a href="#">Playstation 5</a></li>
+            <li><a href="#">Playstation 4</a></li>
+            <li><a href="#">Nintendo Switch</a></li>
+            <li><a href="#">Xbox Series X</a></li>
+            <li><a href="#">Xbox One</a></li>
+            <li><a href="#">Xbox 360</a></li>
+            <li><a href="#">PC</a></li>
+            <li><a href="#">PC Download</a></li>
+            <li><a href="#">Nintendo 3DS</a></li>   
+            
+            </ul>
+        </div>
+
+        <div class="footer__contact">
+             <h2>Kontakt</h2>
+            <a href="#">Seebladsgade 1, 5000 Odense</a>
+            <p>+45 12 34 56 78</p>
+        </div>
+
+        <div class="footer__social">
+            <h2>Social</h2>
+            <div class="social__list">
+                <a href="#" class="fa-facebook fa-xl"><fa :icon= "['fab', 'facebook',]"/></a>
+                <a href="#" class="fa-instagram fa-xl"><fa :icon= "['fab', 'instagram',]"/></a>
+                <a href="#" class="fa-youtube fa-xl"><fa :icon= "['fab', 'youtube',]"/></a>
+                <a href="#" class="fa-twitter fa-xl"><fa :icon= "['fab', 'twitter',]"/></a>
+
+                <!-- <fa :icon= "['fab', 'facebook',]"/><a href="#"></a>
+                <fa :icon= "['fab', 'youtube',]"/><a href="#"></a>
+                <fa :icon= "['fab', 'instagram',]"/><a href="#"></a>
+                <fa :icon= "['fab', 'twitter',]"/><a href="#"></a>
+                 -->
+            </div>
+        </div>
+        </div>
+       <div class="copyrightText">
+           <p>plz dont copy</p>
+       </div>
+    </div>
+</template>
