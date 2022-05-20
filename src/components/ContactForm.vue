b38118d4be6de1864db7eb6cd30f5849288b9798
Laurits92 <34508820+Laurits92@users.noreply.github.com>
2022-05-20 11:46:37 +0200

    backup

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,18 +1,26 @@
 <template>
 <h3>Contact Form</h3>
 <form @submit.prevent="HandleSubmit">
+<label>Navm</label>
+<input type="userName" required v-model="userName">
+
+<label>Telefonnummer</label>
+<input type="phoneNumber" required v-model="phoneNumber">
+
 <label>Email</label>
 <input type="email" required v-model="email">
 
-<label>Password</label>
-<input type="password" required v-model="password">
+<label>Besked</label>
+<input type="message" required v-model="message">
+<div v-if="messageError"> {{ messageError }}</div>
 
 <div class="submit"></div>
 <button>Send Besked</button>
 </form>
 
 <p>Email: {{ email }}</p>
-<p>Password: {{ password }}</p>
+<p>tlf: {{ phoneNumber }}</p>
+<p>besked: {{ message }}</p>
 </template>
 
 
@@ -21,13 +29,21 @@ export default {
     data() {
         return{
             email: '',
-            password: ''
+            password: '',
+            userName: '',
+            phoneNumber: '',
+            message: '',
+            messageError: ''
+            
+            
         }
     },
     methods: {
         handleSubmit() {
-            console.log('form submitted')
-        }
+           /*  console.log('form submitted') ---hvorfor virker den ikke? */
+           // besked validation (skal have flere)
+             this.messageError = this.message.length > 15 ? '' : 'vær sød at specificere problemet '
+         }
     }
     
 }
