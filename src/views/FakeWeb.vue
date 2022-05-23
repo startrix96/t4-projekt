2c00d65b4f652bcdae817059c1b785852e47e5ac
Benjamin Porsgaard <benjamin_porsgaard@hotmail.com>
2022-05-23 18:21:51 +0200

    nye opdateringer

---
+++ src/views/FakeWeb.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Dette er en falsk webshop</h2>
+    <p>
+        Vi henviser til at handle på en anden webshop, da produkterne på denne webshop ikke er
+        virkelige. <br />Vi beklager ulejligheden, samt håber at du finder dine spil et andet sted.
+    </p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/ProductTemplate.vue
+++ src/views/ProductTemplate.vue
@@ -1,68 +1,56 @@
 <template>
- <div>
-  <section>
-    <h2>{{ products.attributes.title }}</h2> 
     <div>
-      <!-- <img
-        :src="product.attributes.thumbnail.data.attributes.url"
-        alt="Product image"
-      /> -->
-      <div>
-        <p>
-          <strong>Description: </strong> <br />
-          {{ products.attributes.description }}
-        </p>
-      </div>
+        <section>
+            <h2>{{ products.attributes.title }}</h2>
+            <div>
+                <div>
+                    <p>
+                        <strong>Description: </strong> <br />
+                        {{ products.attributes.description }}
+                    </p>
+                </div>
+            </div>
+            <div>
+                <p>Price: {{ products.attributes.price }}kr</p>
+                <div>
+                    <!-- <router-link to="/FakeWeb">Køb</router-link> -->
+                    <router-link to="/FakeWeb" custom v-slot="{ navigate }">
+                        <span @click="navigate" @keypress.enter="navigate" role="link">Køb nu</span>
+                    </router-link>
+                </div>
+            </div>
+            <div>
+                <h3>Lagerstatus:</h3>
+                <p>{{ products.attributes.quantity }}</p>
+            </div>
+        </section>
     </div>
-    <div>
-      <p>Price: {{ products.attributes.price }}kr</p>
-      <div>
-        add to cart button
-        <button
-          @click="addToCart(product)"
-        >
-        </button>
-      </div>
-    </div> 
-    <div> 
-      <h3>Lagerstatus:</h3>
-      <p>{{ products.attributes.quantity }}</p>
-    </div>
-
-  </section>
- </div>
- hej
+    hej
 </template>
 
 <script>
 export default {
-   props: ["id"],
+    props: ["id"],
     data() {
         return {
-            products: {attributes: { thumbnail: { data: { attributes: { url: "" } } } },},
+            products: { attributes: { data: { attributes: { url: "" } } } },
             error: null,
         };
-      
-
     },
     async mounted() {
         try {
-          await fetch("https://backend-mark.herokuapp.com/api/products/" +
-          this.$route.params.id +
-          "?populate=%2A")
-            .then(response => response.json())
-            .then(data => (this.products = data.data)); 
-            console.log(this.products)
-
+            await fetch(
+                "https://backend-mark.herokuapp.com/api/products/" +
+                    this.$route.params.id +
+                    "?populate=%2A"
+            )
+                .then(response => response.json())
+                .then(data => (this.products = data.data));
+        } catch (error) {
+            this.error = error;
         }
-        catch (error) {
-      this.error = error;
-    }
-      
-}
-}
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
